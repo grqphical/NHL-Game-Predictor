de30b52ec1ec8adeb60858da66d7b3d4a1874eea
grqphical07 <95062977+grqphical07@users.noreply.github.com>
2024-09-03 11:08:18 -0700

    using random forest as it is more accurate

--- model.py
+++ model.py
@@ -1,47 +1,10 @@
 """Random forest classification model"""
 
-from sklearn.ensemble import RandomForestClassifier, HistGradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import GridSearchCV
 from data_processor import load_and_process_data
 import pandas as pd
 from sklearn.metrics import classification_report
-
-
-def train_gradient_boost_model(data: pd.DataFrame):
-    training_data = data[data["gameDate"].dt.year < 2023]
-    testing_data = data[data["gameDate"].dt.year == 2023]
-
-    predictors = [
-        "home_or_away",
-        "dayOfWeek",
-        "daysSinceLastGame",
-        "goalsAgainst_rolling",
-        "goalsFor_rolling",
-        "shotAttemptsFor_rolling",
-        "penaltiesFor_rolling",
-        "missedShotsFor_rolling",
-        "faceOffsWonFor_rolling",
-        "corsiPercentage_rolling",
-        "homeAdvantage",
-    ]
-
-    model = HistGradientBoostingClassifier(
-        random_state=44,
-        class_weight="balanced",
-        max_depth=8,
-        max_iter=500,
-        learning_rate=0.5,
-        l2_regularization=0.5,
-    )
-
-    model.fit(training_data[predictors], training_data["outcome"])
-
-    preds = model.predict(testing_data[predictors])
-
-    print(accuracy_score(testing_data["outcome"], preds))
-
-    print(classification_report(testing_data["outcome"], preds))
 
 
 def train_random_forest_model(data: pd.DataFrame):
@@ -63,9 +26,7 @@ def train_random_forest_model(data: pd.DataFrame):
     ]
 
     model = RandomForestClassifier(
-        random_state=44,
-        n_estimators=250,
-        n_jobs=10,
+        random_state=44, n_estimators=250, n_jobs=10, class_weight="balanced"
     )
     model.fit(training_data[predictors], training_data["outcome"])
 
@@ -78,4 +39,4 @@ def train_random_forest_model(data: pd.DataFrame):
 
 if __name__ == "__main__":
     data = load_and_process_data("data/all_teams.csv")
-    train_gradient_boost_model(data)
+    train_random_forest_model(data)
